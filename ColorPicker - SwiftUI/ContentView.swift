3d66a2464ab9454bc3b755860168a9222d51c916
Kovalyov Dmitriy <kovdmkonst@gmail.com>
2021-11-06 21:57:14 +1000

    Initial Commit

---
+++ ColorPicker - SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorPicker - SwiftUI
+//
+//  Created by Kovalyov Dmitry on 06.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
